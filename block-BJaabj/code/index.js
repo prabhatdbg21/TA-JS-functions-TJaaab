b2f918b8e275869b920121007abfe679bb6bba69
prabhat <prabhatdbg21@gmail.com>
2022-03-06 17:00:11 +0530

    block-BJaabj

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
 
+function sayHello(){
+  alert ("Hello World!");
+}
+
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -10,6 +14,13 @@
   - Concat the value of fistName and lastName and store it in another variable named `fullName`.
   - Alert `fullName`
 */
+ 
+function fullName(){
+  let firstName = prompt ("Enter your first name");
+  let lastName = prompt ("Enter your last name");
+  var fullName = firstName + lastName ;
+  alert (fullName);
+}
 
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
@@ -19,6 +30,13 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+
+function addTwoNumbers (){
+  let firstNum = +prompt ("Enter your first number");
+  let secondNum = +prompt ("Enter your second name");
+  var sum = firstNum + secondNum;
+  alert (sum) ;
+}
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -39,12 +57,29 @@
   9 * 10 = 90
 */
 
+function getTable (){
+  let num = +prompt("Enter a number");
+  for (i = 1 ; i <= 10 ; ++i){
+    console.log (`${num} * ${i} = ${num * i}`);
+  }
+}
+
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+
+function isLeapYear (){
+  let year = +prompt ("Enter a year");
+  if (year % 4 == 0){
+    alert ("leap year");
+  }
+  else {
+    alert ("not a leap year");
+  }
+}
 
 /*
 6. Create a function named `getFactorial` with the following steps:
@@ -53,3 +88,8 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+
+function getFactorial (){
+  let number = +prompt("Enter a number");
+  alert (`The fraction of ${number} is fraction`)
+}
